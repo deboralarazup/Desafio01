69f8d67a08f72631fe90cc283e01a310afde3de9
Debora Lara <debora.lara@zup.com.br>
2021-05-12 15:33:30 -0300

    [Refatorado] - Subindo Desafio 01 com melhorias de responsividade e fazendo o uso de dados de uma API para montagem da lista/tabela

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-const input = document.getElementById('filtro-nome');
-//fazendo o uso do spread/rest para array
-const trs = [...document.querySelectorAll('#lista tbody tr')];
 
-input.addEventListener('input', () => {
-  const search = input.value.toLowerCase();
-  trs.forEach(el => {
-    const matches = el.textContent.toLowerCase().includes(search);
-    el.style.display = matches ? 'table-row' : 'none';
+const  URL = 'http://localhost:3000/user';
+
+const buildProfile = (profiles) => {
+  const htmlProfile = profiles.map(profile => {
+    const listProfile = `
+      <li class="list-profile" id="profile">
+        <div class="item-profile">
+          <img src="${profile.img}" />
+          <span>${profile.nome}</span>
+        </div>
+        <span class="span-email" title="${profile.email}">${profile.email}</span>
+        <span>${profile.contato}</span>
+        <span>${profile.estado}</span>
+        <div class="table-action">
+          <img src="./img/allselect.svg" />
+          <img src="./img/check-all1.svg" data-email='${profile.email}' onclick="removeUser(this, 'attended')" /> 
+          <img src="./img/trash.svg" data-email='${profile.email}' onclick="removeUser(this, 'remove')" /> 
+        </div>
+      </li>
+    `;
+  
+    return listProfile;
   });
-});
+  
+  return htmlProfile
+}
+
+const getProfile = async() => {
+  const profiles = await fetch(URL)
+  .then((resp)=> resp.json())
+  return profiles
+}
+
+const renderTable = async(profiles) => {
+  const wrapperProfile = document.querySelector('#wrapper-profile');
+  const htmlProfiles = buildProfile(profiles);
+  htmlProfiles.forEach(htmlProfile => wrapperProfile.insertAdjacentHTML('beforeend', htmlProfile));
+}
+
+const renderProfiles = async() => {
+  const profiles = await getProfile()
+  renderTable(profiles)
+}
+
+renderProfiles()
