b9f0389cc0f254228d59e83312d40e28b7e44f6c
Debora Lara <debora.lara@zup.com.br>
2021-06-01 11:35:30 -0300

    [Requisitos] - Ao clicar em um item da listagem apresenta as informações do usuário em uma nova tela

--- index.js
+++ index.js
@@ -148,3 +148,97 @@ const renderList = (list, typeList) => {
   };
 };
 renderProfiles();
+
+// Inicio Tela Detalhes do usuarios
+const buildDetalhes = (listaDetails) => {
+  const htmlDetais = listaDetails.map((listaDetail) => {
+    let listDetais = `
+    <div class="card">
+      <div class="banner">
+        <img class="user" src="${listaDetail.img}" alt="">
+      </div>
+      <h2 class="info-user" id="exemplo">
+        <div class="title">Hi, My name is</div> 
+        ${listaDetail.name}
+      </h2>
+      <div class="actions">
+        <div class="follow-btn">
+              <ul class="lista-icones">
+                  <img src="./img/profile-use.svg" alt="" class="icon" onmouseover="actionsDetails('name');">
+                  <img src="./img/email.svg" alt="" class="icon"  onmouseover="actionsDetails('email')">
+                  <img src="./img/calendar.svg" alt="" class="icon" onmouseover="actionsDetails('birthday')">
+                  <img src="./img/mapa.svg" alt="" class="icon" onmouseover="actionsDetails('city')">
+                  <img src="./img/phone.svg" alt="" class="icon" onmouseover="actionsDetails('phone')">
+                  <img src="./img/key.svg" alt="" class="icon" onmouseover="actionsDetails('password')">
+              </ul>
+        </div>
+      </div>
+    </div>
+    `;
+    return listDetais;
+  });
+  return htmlDetais;
+};
+
+// pegando item e colocando na lista
+const SetDetails = (email, list) => {
+  const removidoProfile = renderDetails(email, list);
+  if(List === 'details'){
+  listaDetails.push(removidoProfile);
+  }
+}   
+
+const renderDetails = (email, List) => {
+  // Pegando a lista
+    if(List === 'details'){
+     //filtrando item clicado
+      listaDetails = listaGlobal.filter((novaTodos) => novaTodos.email === email);
+      document.querySelector(".page").style.display = "none";
+      const wrapperProfile = document.querySelector("#detalhes");
+      wrapperProfile.style.display = "";
+      wrapperProfile.innerHTML = "";
+      const htmlProfiles = buildDetalhes(listaDetails, "details");
+      htmlProfiles.forEach((htmlProfile) =>
+      wrapperProfile.insertAdjacentHTML("beforeend", htmlProfile)
+      )
+    }
+};
+
+function actionsDetails(propriedade) {
+  let person = listaDetails[listaDetails.length - 1];
+  let finalPerson = listaGlobal.filter((el) => el.email == person.email);
+
+  // coloca no html
+  let info = null;
+  info = document.querySelector(".info-user").textContent;
+
+  switch (propriedade) {
+    case "name":
+      info = document.querySelector(".info-user").innerHTML =
+        `<div class="title">Hi, My name is</div>` + finalPerson[0].name;
+      break;
+
+    case "email":
+      info = document.querySelector(".info-user").innerHTML =
+        `<div class="title">My email address is</div>` + finalPerson[0].email;
+      break;
+    case "birthday":
+      info = document.querySelector(".info-user").innerHTML =
+        `<div class="title">My birthday is</div>` + finalPerson[0].birthday;
+      break;
+    case "city":
+      info = document.querySelector(".info-user").innerHTML =
+        `<div class="title">My address is</div>` + finalPerson[0].city;
+      break;
+    case "phone":
+      info = document.querySelector(".info-user").innerHTML =
+        `<div class="title">My phone number is</div>` + finalPerson[0].number;
+      break;
+    case "password":
+      info = document.querySelector(".info-user").innerHTML =
+        `<div class="title">My password is</div>` + finalPerson[0].password;
+      break;
+    default:
+      break;
+  }
+}
